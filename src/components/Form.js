40ec0a0f5e34cb07a158f13c3f47858144d6e9cc
DVitsme <derrick.valentine@gmail.com>
2019-08-05 23:21:28 -0400

    Set up form with icons

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { MdSend } from 'react-icons/md';
 
 const Form = () => {
-  return <div>form</div>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="expense">Product Name</label>
+        <input
+          type="text"
+          id="expense"
+          name="expense"
+          placeholder="e.g. Food"
+        />
+      </div>
+      <div>
+        <label htmlFor="price">Price</label>
+        <input type="text" id="price" name="price" placeholder="e.g. $15" />
+      </div>
+      <button type="submit">
+        Submit <MdSend />
+      </button>
+    </form>
+  );
 };
 
 export default Form;
