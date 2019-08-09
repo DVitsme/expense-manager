edda509804a8b75c6682fa42754821431ba5d251
DVitsme <derrick.valentine@gmail.com>
2019-08-08 23:56:17 -0400

    Display Message on success

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Alert = () => {
-  return <div />;
+const Alert = ({ type, text }) => {
+  return (
+    <div>
+      {type}
+      {text}
+    </div>
+  );
 };
 
 export default Alert;
