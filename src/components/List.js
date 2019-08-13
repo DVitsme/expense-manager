78c3e709bb3965b33ba6dcf5a5cc107915f50e09
DVitsme <derrick.valentine@gmail.com>
2019-08-12 23:08:55 -0400

    Renamed Expense === expensedItem + Pass Edit + Delete functionality

--- src/components/List.js
+++ src/components/List.js
@@ -2,18 +2,23 @@ import React from 'react';
 import Item from './Item';
 import { Button, Card, Icon } from 'semantic-ui-react';
 
-const List = ({ expenses }) => {
+const List = ({ expenses, handleEdit, handleDelete, clearItems }) => {
   return (
     <div className="my-halfEm">
       <div className="my-oneEm">
         <Card.Group centered>
           {expenses.map(expense => (
-            <Item key={expense.id} expense={expense} />
+            <Item
+              key={expense.id}
+              expensedItem={expense}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
           ))}
         </Card.Group>
       </div>
       {expenses.length > 0 && (
-        <Button inverted size="big" color="red">
+        <Button inverted size="big" color="red" onClick={clearItems}>
           Clear Items <Icon name="trash alternate" size="small" />
         </Button>
       )}
