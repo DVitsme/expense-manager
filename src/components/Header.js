4315467a5b63744bd5642bcf8157988a4f73b7fb
DVitsme <derrick.valentine@gmail.com>
2019-08-13 11:25:48 -0400

    Updates header color when edit === true

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Header, Icon } from 'semantic-ui-react';
 import { formatPrice } from '../helpers';
 
-const expenseHeader = ({ expenses }) => {
+// using edit here for ui display purposes, surves no functional purpose
+const expenseHeader = ({ expenses, edit }) => {
   const amount = expenses.reduce((acc, cur) => {
     return (acc += cur.price);
   }, 0);
@@ -14,7 +15,7 @@ const expenseHeader = ({ expenses }) => {
           Expense Manager
         </Header>
       ) : (
-        <Header as="h2" color="teal" textAlign="center">
+        <Header as="h2" color={edit ? 'yellow' : 'teal'} textAlign="center">
           <Icon name="money bill alternate outline" size="big" />
           Total Amount Spent: {formatPrice(amount)}
         </Header>
